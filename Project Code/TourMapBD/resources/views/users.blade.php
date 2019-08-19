cc8c7bcafa48097745fcc4a0eca63f4cb47afcc8
Masrur Ahmed Santo <51715637+santo6774@users.noreply.github.com>
2019-08-19 19:54:12 +0600

    Feature #28 : users.blade.php
    users containers class created

---
+++ Project Code/TourMapBD/resources/views/users.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-2" style="background-color: #fff;">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection

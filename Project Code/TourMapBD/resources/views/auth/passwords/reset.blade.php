5b06d215f59b80e56a9fff55f3b290ecd548e431
Masrur Ahmed Santo <51715637+santo6774@users.noreply.github.com>
2019-08-19 23:12:12 +0600

    Feature #24 : reset.blade.php
    submit button

--- Project Code/TourMapBD/resources/views/auth/passwords/reset.blade.php
+++ Project Code/TourMapBD/resources/views/auth/passwords/reset.blade.php
@@ -44,6 +44,13 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Reset Password') }}
+                                </button>
+                            </div>
+                        </div>
 
                         
                     </form>

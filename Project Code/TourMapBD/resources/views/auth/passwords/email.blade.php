42be29bb2df857697252db4b129a83b5ff63fb6e
Masrur Ahmed Santo <51715637+santo6774@users.noreply.github.com>
2019-08-19 23:54:40 +0600

    Feature #23 : email.blade.php
    email address

--- Project Code/TourMapBD/resources/views/auth/passwords/email.blade.php
+++ Project Code/TourMapBD/resources/views/auth/passwords/email.blade.php
@@ -12,6 +12,17 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            </div>
+                        </div>
+                    </form>
              </div>
         </div>
     </div>

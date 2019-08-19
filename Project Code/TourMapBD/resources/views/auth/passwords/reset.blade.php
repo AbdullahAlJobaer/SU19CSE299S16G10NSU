821e4f370d34a9812dfa79996844b61a39ff4dfb
Masrur Ahmed Santo <51715637+santo6774@users.noreply.github.com>
2019-08-19 23:14:53 +0600

    Feature #24 : reset.blade.php
    bug solved

--- Project Code/TourMapBD/resources/views/auth/passwords/reset.blade.php
+++ Project Code/TourMapBD/resources/views/auth/passwords/reset.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
-                        
+
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
@@ -24,8 +24,10 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+                            </div>
+                        </div>
 
-                                <div class="form-group row">
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -36,14 +38,17 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+                            </div>
+                        </div>
 
-                                <div class="form-group row">
+                        <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -51,8 +56,6 @@
                                 </button>
                             </div>
                         </div>
-
-                        
                     </form>
                 </div>
             </div>
@@ -60,3 +63,4 @@
     </div>
 </div>
 @endsection
+

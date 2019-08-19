fe129e20c0e94aaf16f3e959ce31af6b19941cf4
Masrur Ahmed Santo <51715637+santo6774@users.noreply.github.com>
2019-08-19 23:01:34 +0600

    Feature #24 : reset.blade.php
    password error resolved

--- Project Code/TourMapBD/resources/views/auth/passwords/reset.blade.php
+++ Project Code/TourMapBD/resources/views/auth/passwords/reset.blade.php
@@ -31,6 +31,11 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
 
                         
                     </form>

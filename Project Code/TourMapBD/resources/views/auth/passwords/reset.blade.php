ad0284af290cf7f25a6cfdbd970aae6ba88fbf58
Masrur Ahmed Santo <51715637+santo6774@users.noreply.github.com>
2019-08-19 22:31:00 +0600

    Feature #24 : reset.blade.php
    password update routed

--- Project Code/TourMapBD/resources/views/auth/passwords/reset.blade.php
+++ Project Code/TourMapBD/resources/views/auth/passwords/reset.blade.php
@@ -7,7 +7,13 @@
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
-               
+                <div class="card-body">
+                    <form method="POST" action="{{ route('password.update') }}">
+                        @csrf
+
+                        
+                    </form>
+                </div>
             </div>
         </div>
     </div>

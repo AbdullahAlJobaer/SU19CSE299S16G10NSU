254bbf51b2ff43dbda45d4e0d45f681b0fe0177b
Masrur Ahmed Santo <51715637+santo6774@users.noreply.github.com>
2019-08-21 00:28:28 +0600

    Feature #31 : PaymentController.php
    stripe test key copied

--- Project Code/TourMapBD/app/Http/Controllers/PaymentController.php
+++ Project Code/TourMapBD/app/Http/Controllers/PaymentController.php
@@ -9,5 +9,7 @@ use App\User;
 
 class PaymentController extends Controller{
     public function pay(Request $request){
+        \Stripe\Stripe::setApiKey('sk_test_zXN6iqBQnZ8FX4xKzHBoX7DG00gaRzlHWA');
+        $token = $request->stripeToken;
     }
 }

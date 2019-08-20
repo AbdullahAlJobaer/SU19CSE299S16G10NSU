e619d12562d813a429cab16c04774070dcb8474b
Masrur Ahmed Santo <51715637+santo6774@users.noreply.github.com>
2019-08-21 00:32:15 +0600

    Update PaymentController.php
    stripe try catch added and payment status updated

--- Project Code/TourMapBD/app/Http/Controllers/PaymentController.php
+++ Project Code/TourMapBD/app/Http/Controllers/PaymentController.php
@@ -11,5 +11,21 @@ class PaymentController extends Controller{
     public function pay(Request $request){
         \Stripe\Stripe::setApiKey('sk_test_zXN6iqBQnZ8FX4xKzHBoX7DG00gaRzlHWA');
         $token = $request->stripeToken;
+        try{
+            $charge = \Stripe\Charge::create([
+                'amount' => $request->amount * 100,
+                'currency' => 'usd',
+                'description' => 'Example charge',
+                'source' => $token,
+            ]);
+            return back()->with('status','Payment successful.');
+        } catch(\Stripe\Error\Base $e){
+            return back()->with('status','Payment Unsuccessful. '.$e->getMessage());
+        }
+
+    }
+
+    public function create(){
+        return view('payment');
     }
 }

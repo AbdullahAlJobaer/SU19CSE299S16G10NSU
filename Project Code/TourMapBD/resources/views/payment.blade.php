b0622ffa485519ecf6b5da0584fc58c82d56a7c9
Masrur Ahmed Santo <51715637+santo6774@users.noreply.github.com>
2019-08-20 23:31:41 +0600

    Feature #30 : payment.blade.php
    card for payment

--- Project Code/TourMapBD/resources/views/payment.blade.php
+++ Project Code/TourMapBD/resources/views/payment.blade.php
@@ -34,3 +34,16 @@
   background-color: #fefde5 !important;
 }
 </style>
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"> Personal Information</div>
+
+               
+            </div>
+        </div>
+    </div>
+</div>
